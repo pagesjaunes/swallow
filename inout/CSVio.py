78047d3dea55a52420c20b2799b4db71f6555c58
fdepaulis <fdepaulis.externe@pagesjaunes.fr>
2014-08-26 17:54:00 +0200

    read from csv

--- inout/CSVio.py
+++ inout/CSVio.py
@@ -1,30 +1,28 @@
-from swallow.settings import logger, EXIT_IO_ERROR, EXIT_USER_INTERRUPT
+from swallow.settings import logger, EXIT_USER_INTERRUPT
 import sys
 import csv
 
 class CSVio: 
     """Reads and Writes documents from/to csv file"""
 
-    def __init__(self,p_csv_file):
-        """Class creation
+    def __init__(self):
+        # Contains all file cursors
+        self.csvfilecursor = {}
+        self.out_csvfile = {}
 
-            Attributes :
-            - csvfile:         file cursor
-            - out_csvfile:    csv file object
-        """
-        try:
-            self.csvfile = open(p_csv_file, "w")
-            self.out_csvfile = csv.writer(self.csvfile,delimiter=';',quotechar='"',quoting=csv.QUOTE_NONNUMERIC)
-        except:
-            logger.error("CSVio : can't open %s for writing",p_csv_file)
-            sys.exit(EXIT_IO_ERROR)
-
-    def dequeue_and_store(self,p_queue):
+    def dequeue_and_store(self,p_queue,p_file,p_delimiter=',',p_quotechar='"',p_quoting=csv.QUOTE_NONNUMERIC):
         """Gets docs from p_queue and stores them in the csv file
              Stops dealing with the queue when receiving a "None" item
 
-            p_queue: queue wich items are picked from. Elements has to be "list".
+            p_queue:    queue wich items are picked from. Elements has to be "list".
+            p_file:     file to store in
         """
+
+        # If not exists, creates the cursor
+        if p_file not in self.csvfilecursor:
+            self.csvfilecursor[p_file] = open(p_file, "w")
+            self.out_csvfile[p_file] = csv.writer(self.csvfilecursor[p_file],delimiter=p_delimiter,quotechar=p_quotechar,quoting=p_quoting)
+
         # Loop untill receiving the "poison pill" item (meaning : no more element to read)
         poison_pill = False
         while not(poison_pill):
@@ -39,11 +37,34 @@ class CSVio:
                     p_queue.task_done()
                     break
 
-                self.out_csvfile.writerow(source_doc)
+                self.out_csvfile[p_file].writerow(source_doc)
 
                 p_queue.task_done()
             except KeyboardInterrupt:
                 logger.info("CSVio.dequeue_and_store : User interruption of the process")
-                self.csvfile.close()
+                self.csvfilecursor[p_file].close()
                 sys.exit(EXIT_USER_INTERRUPT)
 
+    def scan_and_queue(self,p_queue,p_file,p_delimiter=',',p_skip_header=True):        
+        """Reads csv file and pushes each line to the queue
+            
+            p_queue:    Queue where items are pushed to
+            p_file:     CSV File to scan
+            p_skip_header: Don't pass the first line
+        """
+        logger.info('Scanning csv in %s', p_file)
+
+        # cr = csv.reader(open(p_file,"r",delimiter=p_delimiter))
+        # for row in cr:
+        #     print (row)
+
+        filecursor = open(p_file, 'r')
+        reader = csv.reader(filecursor,delimiter=p_delimiter)
+
+        # Skip first line ?
+        skipline = p_skip_header
+        for row in reader:
+            if not skipline:
+                p_queue.put(row)
+            else:
+                skipline = False
