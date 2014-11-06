d2c260e536eec6cd2c5f10bd08ab8c0e58333023
fdepaulis <fdepaulis.externe@pagesjaunes.fr>
2014-11-06 17:21:52 +0100

    increase default queue size

--- Swallow.py
+++ Swallow.py
@@ -42,7 +42,7 @@ class Swallow:
             swal.run(nb_threads)
     """
 
-    def __init__(self, p_max_items_by_queue=20000):
+    def __init__(self, p_max_items_by_queue=50000):
         """Class creation"""
 
         self.readers = None
