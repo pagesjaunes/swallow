fbd9252a83740e135d644bbe6847be97f8867eb8
fdepaulis <fdepaulis.externe@pagesjaunes.fr>
2015-01-14 17:28:59 +0100

    refactoring for setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 """Setup script for swallow distributions"""
 
 from setuptools import setup, find_packages
-import 
+import swallow
 
 def main():
     setup(
         name = 'swallow',
-        version = ElasticSearchLib.__version__,
-        description = 'ElasticSearch library for Robot Framework',
+        version = swallow.__version__,
+        description = 'Swallow for data transformation',
         author = 'PagesJaunes',
         author_email = 'fdepaulis@pagesjaunes.fr',
         url = 'https://github.com/pagesjaunes/swallow',
         packages=find_packages(),
-        install_requires = ['robotframework','elasticsearch']
+        install_requires = ['algoliasearch','elasticsearch','pymongo']
     )
 
 if __name__ == "__main__":
