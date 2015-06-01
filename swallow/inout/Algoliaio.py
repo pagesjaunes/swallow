b5c4952d3589faa19feaa8bcf6320d3665019aa6
Fabrice Depaulis <fdepaulis@pagesjaunes.fr>
2015-06-01 12:03:26 +0200

    New algolia client compatibility + retry mecanism on bulk indexing

--- swallow/inout/Algoliaio.py
+++ swallow/inout/Algoliaio.py
@@ -16,6 +16,7 @@ class Algoliaio:
         self.api_key = p_api_key
         self.bulk_size = p_bulksize
 
+
     def clear_index(self,p_index):
         """Deletes and index
 
@@ -26,8 +27,8 @@ class Algoliaio:
 
         try:
             client = algoliasearch.Client(self.app_id,self.api_key)
-            index = client.initIndex(p_index)
-            index.clearIndex()
+            index = client.init_index(p_index)
+            index.clear_index()
             logger.info('Index %s deleted',p_index)
         except Exception as e:
             logger.error('Error deleting the index %s',p_index)
@@ -35,6 +36,7 @@ class Algoliaio:
             delete_ok = False
 
         return delete_ok
+
 
     def delete_document(self,p_index,p_id):
         """Deletes a doc from an index
@@ -45,8 +47,8 @@ class Algoliaio:
 
         try:
             client = algoliasearch.Client(self.app_id,self.api_key)
-            index = client.initIndex(p_index)
-            index.deleteObject(p_id)
+            index = client.init_index(p_index)
+            index.delete_object(p_id)
             logger.info('%s deleted from %s',p_id, p_index)
         except Exception as e:
             logger.error('Error deleting the %s from index %s',p_id, p_index)
@@ -61,23 +63,25 @@ class Algoliaio:
         """
         try:
             client = algoliasearch.Client(self.app_id,self.api_key)
-            index = client.initIndex(p_index)
-            index.setSettings(p_conf)
+            index = client.init_index(p_index)
+            index.set_settings(p_conf)
             logger.info('Index %s set',p_index)
         except Exception as e:
             logger.error('Error setting the index %s',p_index)
             logger.error(e)
 
-    def dequeue_and_store(self,p_queue,p_index):
+
+    def dequeue_and_store(self,p_queue,p_index,p_nbmax_retry=3):
         """Gets docs from p_queue and stores them in the algolia
              Stops dealing with the queue when receiving a "None" item
 
             p_queue:             queue wich items are picked from. Elements has to be "list".
             p_index:            algolia index where to store the docs
+            p_nbmax_retry:      number of tries when failing on a request (default is 3)
         """
 
         client = algoliasearch.Client(self.app_id,self.api_key)
-        index = client.initIndex(p_index)
+        index = client.init_index(p_index)
 
         # Loop untill receiving the "poison pill" item (meaning : no more element to read)
         poison_pill = False
@@ -97,41 +101,24 @@ class Algoliaio:
                     bulk.append(source_doc)
                     p_queue.task_done()
 
-                try:
-                    # Bulk indexation
-                    if len(bulk) > 0:
-                        logger.debug("Indexing %i documents",len(bulk))
-                        index.addObjects(bulk)
-                except Exception as e:
-                    logger.error("Bulk not indexed in algolia")
-                    logger.error(e)
+                try_counter = 1
+                is_indexed = False
+                while try_counter <= p_nbmax_retry and not is_indexed:
+                    try:
+                        # Bulk indexation
+                        if len(bulk) > 0:
+                            logger.debug("Indexing %i documents",len(bulk))
+                            index.add_objects(bulk)
+                    except Exception as e:
+                        logger.error("Bulk not indexed in algolia - Retry number %i",try_counter)
+                        logger.error(e)
+                        try_counter += 1
+                    else:
+                        is_indexed = True
+
+                if not is_indexed:
+                    logger.error("Bulk not indexed in algolia : operation aborted after %i retries",try_counter-1)
+
             except KeyboardInterrupt:
                 logger.info("ESio.dequeue_and_store : User interruption of the process")
                 sys.exit(1)
-
-    # def scan_and_queue(self,p_queue,p_index,p_query={},p_doctype=None,p_scroll_time='60m',p_timeout='60m'):
-    #     """Reads docs from an es index according to a query and pushes them to the queue
-
-    #         p_queue:         Queue where items are pushed to
-    #         p_scroll_time:    Time for scroll method
-    #         p_timeout:        Timeout - After this period, scan context is closed
-    #         p_index:        Index where items are picked from
-    #         p_doctype:        DocType of the items
-    #         p_query:        ElasticSearch query for scanning the index
-    #     """
-    #     try:
-    #         param = [{'host':self.host,'port':self.port}]
-    #         es = Elasticsearch(param)
-    #         logger.info('Connected to ES Server for reading: %s',json.dumps(param))
-    #     except Exception as e:
-    #         logger.error('Connection failed to ES Server for reading: %s',json.dumps(param))
-    #         logger.error(e)
-    #         sys.exit(EXIT_IO_ERROR)
-
-    #     if 'p_doctype' is not None:
-    #         documents = helpers.scan(client=es, query=p_query, scroll=p_scroll_time, index=p_index, doc_type=p_doctype, timeout=p_timeout)
-    #     else:
-    #         documents = helpers.scan(client=es, query=p_query, scroll= p_scroll_time, index=p_index, timeout=p_timeout)
-    #     for doc in documents:
-    #         logger.debug(doc)
-    #         p_queue.put(doc)
