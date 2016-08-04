d9d4276c31b5e2fbdbbd77df3057db606ea75aaa
Fabrice Depaulis <fdepaulis@pagesjaunes.fr>
2016-08-04 17:26:12 +0200

    Multiprocessing logger

---
+++ swallow/logger_mp.py
@@ -0,0 +1,16 @@
+import multiprocessing
+from logging.handlers import QueueHandler
+import logging
+
+
+def get_logger_mp(p_name, p_log_queue, p_log_level, p_formatter):
+    '''
+        Init a logger for threads launched in multiprocessing mode
+    '''
+    qh = QueueHandler(p_log_queue)
+    qh.setFormatter(p_formatter)
+    logger = logging.getLogger(p_name)
+    logger.setLevel(p_log_level)
+    logger.addHandler(qh)
+
+    return logger
