5e2cad84d6ae718acea3f54d68d9dd4014ae0120
Fabrice Depaulis <fdepaulis@pagesjaunes.fr>
2016-08-09 11:48:09 +0200

    Update requirements for elasticsearch

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@
 from setuptools import setup, find_packages
 import swallow
 
+
 def main():
     setup(
-        name = 'swallow',
-        version = swallow.__version__,
-        description = 'Swallow for data transformation',
-        author = 'PagesJaunes',
-        author_email = 'fdepaulis@pagesjaunes.fr',
-        url = 'https://github.com/pagesjaunes/swallow',
+        name='swallow',
+        version=swallow.__version__,
+        description='Swallow for data transformation',
+        author='PagesJaunes',
+        author_email='fdepaulis@pagesjaunes.fr',
+        url='https://github.com/pagesjaunes/swallow',
         packages=find_packages(),
-        install_requires = ["algoliasearch >= 1.5.0, < 2","elasticsearch >= 1.5.0, < 2","pymongo >= 3.0.0, < 4","PyMySQL >= 0.6.6, < 1"]
+        install_requires=["algoliasearch >= 1.5.0, < 2", "elasticsearch >= 2.3.0, < 3", "pymongo >= 3.0.0, < 4", "PyMySQL >= 0.6.6, < 1"]
     )
 
 if __name__ == "__main__":
